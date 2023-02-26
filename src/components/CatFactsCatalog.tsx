d6fe02d6f822e9190e97ebfc7cad7fd2bcef502f
Ryan Miller <miller@redhat.com>
2023-02-25 23:32:26 -0500

    Watch for CatFacts and (attempt to) render them
    There's an issue at the moment where CatFacts aren't displayed
    correctly. Maybe I need to trigger React to redraw or something. The
    CatFacts are coming in ok and being logged correctly though! Committing
    this before I mess up everything trying to fix it. 😅

--- src/components/CatFactsCatalog.tsx
+++ src/components/CatFactsCatalog.tsx
@@ -8,25 +8,16 @@ import {
   Title,
 } from '@patternfly/react-core';
 import { CatalogTile } from '@patternfly/react-catalog-view-extension';
+import { CatFact, catFactKind } from './data/model';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 import './example.css';
 
-export type CatFact = {
-  id: string;
-  fact: string;
-};
+// export type CatFact = {
+//   id: string;
+//   fact: string;
+// };
 
 export default function ExamplePage() {
-  const catFactsList: CatFact[] = [
-    {
-      id: 'my-cat-fact',
-      fact: 'Cats are cool!',
-    },
-    {
-      id: 'second-cat-fact',
-      fact: 'Cats are the coolest!!!',
-    },
-  ];
-
   // https://www.patternfly.org/v4/extensions/catalog-view/catalog-tile
   return (
     <>
@@ -37,23 +28,71 @@ export default function ExamplePage() {
         <PageSection variant="light">
           <Title headingLevel="h1">Cat Facts!</Title>
         </PageSection>
-        <PageSection variant="light">
-          {catFactsList.map((item, index) => {
-            return (
-              <CatalogTile
-                key={index}
-                id={item.id}
-                // iconImg={pfLogo2}
-                iconAlt="PatternFly logo"
-                badges={['Badge']}
-                title="Cat Fact"
-                vendor="powered by Cat Facts Operator"
-                description={item.fact}
-              />
-            );
-          })}
-        </PageSection>
+        <CatFactCatalog />
       </Page>
+    </>
+  );
+}
+
+function CatFactCatalog() {
+  const [catFacts, loaded, loadError] = useK8sWatchResource<CatFact[]>({
+    groupVersionKind: catFactKind,
+    isList: true,
+    namespace: 'cat-facts-operator',
+    namespaced: true,
+  });
+
+  console.log({ catFacts });
+  console.log({ loaded });
+  console.log({ loadError });
+
+  if (loaded === false) {
+    return (
+      <>
+        <PageSection variant="light">Loading...</PageSection>
+      </>
+    );
+  }
+
+  if (loadError) {
+    return (
+      <>
+        <PageSection variant="light">ERROR: {loadError}</PageSection>
+      </>
+    );
+  }
+
+  if (loaded === true && catFacts.length === 0) {
+    return (
+      <>
+        <PageSection variant="light">
+          No CatFacts found in cat-facts-operator namespace!
+        </PageSection>
+      </>
+    );
+  }
+
+  // This logs fine
+  console.log(catFacts[0].metadata.name);
+  console.log(catFacts[0].spec.fact);
+
+  // This renders a blank page after loading with no errors
+  return (
+    <>
+      <PageSection variant="light">
+        {catFacts.map((item, index) => {
+          <CatalogTile
+            key={index}
+            id={item.metadata.name}
+            // iconImg={pfLogo2}
+            iconAlt="PatternFly logo"
+            badges={['Badge']}
+            title="Cat Fact"
+            vendor="powered by Cat Facts Operator"
+            description={item.spec.fact}
+          />;
+        })}
+      </PageSection>
     </>
   );
 }
