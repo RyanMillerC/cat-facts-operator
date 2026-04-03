dc28520598e2f04abd1c6733a53e356289bc7f0d
Ryan Miller <RyanMillerC@outlook.com>
2026-04-03 16:41:32 -0400

    Remove ./console (replaced with ./console-plugin)

--- console/integration-tests/support/index.ts
+++
@@ -1,7 +0,0 @@
-// Import commands.js using ES2015 syntax:
-import './login';
-
-export const checkErrors = () =>
-  cy.window().then((win: any) => {
-    assert.isTrue(!win.windowError, win.windowError);
-  });
